86cd2f2ada6e02bd4a135e0d8fbc424869704622
Lorena Alejandra Cespedes Martinez <lcespedes@ine.gov.bo>
2025-02-04 11:33:49 -0400

    lorena - reasignacion

--- controllers/system/validar.controller.js
+++ controllers/system/validar.controller.js
@@ -194,7 +194,7 @@ const getListado = async (req, res) => {
   var ae = req.params.ae;
 
   var query = `select row_number() over(order by a.cod_cuest) nro, a.*, to_char(ca.fecha_asignacion, 'dd-mm-yyyy')fecha_asig, 
-            concat(COALESCE(vu.aut_us_nombres,null),' ',COALESCE (vu.aut_us_paterno,null),' ',COALESCE (vu.aut_us_materno, null)) nombre, ca.estado_id, ce.estado
+            concat(COALESCE(vu.aut_us_nombres,null),' ',COALESCE (vu.aut_us_paterno,null),' ',COALESCE (vu.aut_us_materno, null)) nombre, ca.estado_id, ce.estado,
             ca.estado_id, ce.estado
             from(select distinct * from autenticacion.vw_calidad_filtro vcf where `; 
   // console.log(depto, mpio, ag, ae, emp);
@@ -203,7 +203,10 @@ const getListado = async (req, res) => {
   query += mpio != "null" ? ` and cod_municipio = '${mpio}' ` : "";
   query += ag != "null" ? ` and ag_unico = '${ag}' ` : "";
   query += ae != "null" ? ` and ae_unico = '${ae}' ` : "";
-  query += `)a left join calidad.cal_asignacion ca on ca.rep_id = a.rep_id
+  query += `)a left join (SELECT * FROM (
+            SELECT *, ROW_NUMBER() OVER (PARTITION BY rep_id ORDER BY id DESC) AS rn
+            FROM calidad.cal_asignacion) a
+            WHERE a.rn = 1) ca on ca.rep_id = a.rep_id
           left join autenticacion.vw_usuarios vu on vu.aut_id_usuario = ca.usu_asig_id 
           left join calidad.cal_estado ce on ce.id_estado = ca.estado_id`;
   // console.log(query, '<== query')
@@ -344,6 +347,7 @@ const saveValidar = async (req, res) => {
     });
   }
 };
+
 const saveAsignar = async (req, res) => {
   // console.log(req.body);
   
--- routes/asignacion.routes.js
+++ routes/asignacion.routes.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 
 const {
-    getUser,
+    getUser, userCuestionario, reasignar
 } = require('../controllers/system/asignacion.controller');
 
 const router = Router();
 
 router.get('/getUser', getUser);
+router.get('/usuario-cuest/:usu_id', userCuestionario);
+router.post('/reasignar', reasignar);
 
 
 module.exports = router;
+ 
